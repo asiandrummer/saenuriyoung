7e9aec5973e7760e927e35f5ca214e9d28ad36f8
Hyohyeon Jeong <asiandrummer@gmail.com>
2014-05-30 23:12:57 +0000

    Asynchronously fetching from FB using Koala Gem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,26 @@
 class HomeController < ApplicationController
 
   def index
+  end
+
+  def fb_group_feeds
     app_id = '534511819991113'
     app_secret = '3e9249521fe36343056a365bc99a63d4'
 
     @access_token = Koala::Facebook::OAuth.new(app_id, app_secret).get_app_access_token
     @graph = Koala::Facebook::API.new(@access_token)
 
-    @groups = @graph.get_connections('164196650355754', 'feed')
-    @groups.each do |group|
-      if group["type"] == "photo"
-        group["object"] = @graph.get_object(group["object_id"])
-        group["picture_object"] = group["object"]["images"].find {|image| image["height"] > 600}
+    @feeds = @graph.get_connections('164196650355754', 'feed')
+    @feeds.each do |feed|
+      feed["person"] = @graph.get_object(feed["from"]["id"])
+      feed["person"]["picture"] = @graph.get_picture(feed["from"]["id"])
+      if feed["type"] == "photo"
+        feed["object"] = @graph.get_object(feed["object_id"])
+        feed["picture_object"] = feed["object"]["images"].find {|image| image["height"] > 600}
       end
     end
+
+    render :layout => false
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get  'fb_group_feeds' => 'home#fb_group_feeds'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
